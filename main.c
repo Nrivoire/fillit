7e09abf43a56d96957e2f2b7d2d9665240c3cc1a
Natacha Rivoire <nrivoire@z3r3p2.le-101.fr>
2019-06-25 13:29:12 +0200

    usage et un leak oublie

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nrivoire <nrivoire@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/06/05 15:21:57 by nrivoire     #+#   ##    ##    #+#       */
-/*   Updated: 2019/06/17 21:40:10 by nrivoire    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/06/25 13:24:55 by nrivoire    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -27,29 +27,35 @@ void			print(char *square, int sq_len)
 	}
 }
 
-int				main(int av, char **ac)
+void			make_map(t_fillit *some)
 {
-	int				nb_te;
-	int				enlarge;
-	t_fillit		*some;
 	t_ptr			*tmp;
 
-	av = 2;
-	nb_te = 0;
-	enlarge = 0;
-	if (!(some = (t_fillit *)malloc(sizeof(t_fillit))))
-		return (0);
-	some->lst = NULL;
-	some = read_fillit(some, ac, nb_te);
 	some->map_size = some->sq_area * some->sq_area;
 	do_square(some);
 	tmp = some->lst;
 	while ((optimization(some) != 1) && some->sq_area < 13)
 	{
 		some->map_size = ++some->sq_area * some->sq_area;
+		free(some->square);
 		do_square(some);
 	}
 	lstdel(tmp);
+}
+
+int				main(int av, char **ac)
+{
+	int				nb_te;
+	t_fillit		*some;
+
+	if (av != 2)
+		ft_error("usage : ./fillit sample_fillit");
+	nb_te = 0;
+	if (!(some = (t_fillit *)malloc(sizeof(t_fillit))))
+		return (0);
+	some->lst = NULL;
+	some = read_fillit(some, ac, nb_te);
+	make_map(some);
 	print(some->square, some->sq_area);
 	free(some->square);
 	free(some);
